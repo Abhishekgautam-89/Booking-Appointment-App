e45fcebe676f35005b500190d49f336fae6e5e0e
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-09-13 15:10:12 +0530

    Commit for task11

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ submit.addEventListener('click', onSubmit);
 function onSubmit(e){
 var name1 = document.getElementById('name').value;
 var email = document.getElementById('email').value;
+var phone1 = document.getElementById('phone').value;
+
+var obj = {
+name1,
+email,
+phone1
+};
+var objConverted = JSON.stringify(obj);
 e.preventDefault();
-localStorage.setItem(email, name1);
+localStorage.setItem("User Details", objConverted);
 }
