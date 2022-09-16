a7c054972a08cebd3915a8a0544cc458186456d4
Abhishekgautam-89 <abhishek.gautam89@outlook.com>
2022-09-16 14:06:26 +0530

    Commit for Task13

--- main.js
+++ main.js
@@ -26,8 +26,40 @@ localStorage.setItem(obj.email, objConverted);
 showDataOnScreen(obj);
 }
 function showDataOnScreen(user){
+    if(localStorage.getItem(user.email) !=null){
+        removeUserFromScreen(user.email);
+
+    }
+
     const li = document.createElement('li');
-    li.id = 'userList';
+    li.id = user.email;
     li.appendChild(document.createTextNode(user.name+' '+user.email));
     list.appendChild(li);
+    const edit = document.createElement('input');
+    edit.id ='edit';
+    edit.type='button';
+    edit.value='Edit';
+    li.appendChild(edit);
+    edit.addEventListener('click', ()=>{
+        document.getElementById('name').value=user.name;
+        document.getElementById('email').value = user.email;
+        li.remove();
+    });
+    const del = document.createElement('input');
+    del.id = 'delete';
+    del.value='Delete';
+    del.type='button';
+    del.addEventListener('click', ()=>{
+        localStorage.removeItem(user.email);
+       li.remove(); 
+    });
+    li.appendChild(del);
+}
+
+function removeUserFromScreen(emailId){
+    console.log(emailId);
+    const childNode = document.getElementById(emailId);
+    if(childNode){
+        list.removeChild(childNode);
+    }
 }
